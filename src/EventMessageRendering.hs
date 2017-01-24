20061ece34b551fe7e8297cf90217efdaa4a3e78
Ulf Sauer <usauer@epages.com>
2017-01-24 17:18:31 +0100

    Move event message rendering to separate module
    and minor refactoring

---
+++ src/EventMessageRendering.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module EventMessageRendering
+    ( renderMessageText
+    ) where
+
+import           Data.Monoid
+import           Data.Text        (Text)
+import qualified Data.Text        as T
+
+import           GithubApi
+import           MessageRendering as Msg
+
+-- ----------------------------------------------
+
+renderMessageText :: Event -> Text
+renderMessageText event
+  = case event of
+    PushEvent ref commits headCommit compare repository ->
+      repoPrefix repository
+       <> "Push ("
+       <> optBranch ref
+       <> (T.pack . show . length $ commits) <> "): "
+       <> link (quote (shortenCommitMessage $ cmtMessage headCommit)) compare
+    PullRequestEvent action number (PullRequest htmlUrl state title) repository ->
+      repoPrefix repository
+       <> link ("Pull Request #" <> (T.pack . show) number <> " - " <> state) htmlUrl
+       <> " " <> italic action <> ": "
+       <> title
+    StatusEvent sha state description statusUrl repository ->
+      repoPrefix repository
+       <> link ("Status: " <> state) statusUrl
+       <> ": " <> description
+    CommentEvent action (Issue state issueHtmlUrl issueUser) (Comment commentHtmlUrl commentBody commentUser) repository ->
+      repoPrefix repository
+       <> link ("Comment " <> italic action <> " (" <> usrLogin commentUser <> ")") commentHtmlUrl
+       <> ": " <> shortenCommentMessage commentBody
+  where
+    -- XXX: hardcoded master branch, use payload default branch data
+  optBranch ref = if ref /= "refs/heads/master" then "on " <> lastSegment ref <> ", " else ""
+  lastSegment = last . T.splitOn "/"
+  repoPrefix repo = "[" <> link (repName repo) (repHtml_url repo) <> "] "
+  shortenCommitMessage = shorten 50 "..." . firstLine
+  shortenCommentMessage = shorten 72 "..." . firstLine -- XXX: cuts off multilines without marker
+  firstLine = T.takeWhile (/= '\n')
--- src/MattermostApi.hs
+++ src/MattermostApi.hs
@@ -26,8 +26,8 @@ import           Servant
 import           Servant.Client
 
 import           App
+import           EventMessageRendering
 import           GithubApi
-import           MessageRendering           as Msg
 
 -- ----------------------------------------------
 
@@ -79,36 +79,3 @@ postEvent e = do
     , username = Just "GitHub"
     , icon_url = Just "http://i.imgur.com/NQA4pPs.png"
     }
-
-
--- TODO: proper markdown renderer, more data.text.builder
-renderMessageText :: Event -> Text
-renderMessageText event
-  = case event of
-    PushEvent ref commits headCommit compare repository ->
-      "[" <> link (repName repository) (repHtml_url repository) <> "] "
-       <> "Push ("
-       <> optBranch ref
-       <> (T.pack . show . length $ commits) <> "): "
-       <> link (quote (shortenCommitMessage $ cmtMessage headCommit)) compare
-    PullRequestEvent action number (PullRequest htmlUrl state title) repository ->
-      repoPrefix repository
-       <> link ("Pull Request #" <> (T.pack . show) number <> " - " <> state) htmlUrl
-       <> " " <> italic action <> ": "
-       <> title
-    StatusEvent sha state description statusUrl repository ->
-      repoPrefix repository
-       <> link ("Status: " <> state) statusUrl
-       <> ": " <> description
-    CommentEvent action (Issue state issueHtmlUrl issueUser) (Comment commentHtmlUrl commentBody commentUser) repository ->
-      repoPrefix repository
-       <> link ("Comment " <> italic action <> " (" <> usrLogin commentUser <> ")") commentHtmlUrl
-       <> ": " <> shortenCommentMessage commentBody
-  where
-    -- XXX: hardcoded master branch, use payload default branch data
-  optBranch ref = if ref /= "refs/heads/master" then "on " <> lastSegment ref <> ", " else ""
-  lastSegment = last . T.splitOn "/"
-  repoPrefix repo = "[" <> link (repName repo) (repHtml_url repo) <> "] "
-  shortenCommitMessage = shorten 50 "..." . firstLine
-  shortenCommentMessage = shorten 72 "..." . firstLine -- XXX: cuts off multilines without marker
-  firstLine = T.takeWhile (/= '\n')
