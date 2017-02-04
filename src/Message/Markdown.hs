81046ab3f2ea49e61311365b9e41f49574165028
Ulf Sauer <usauer@epages.com>
2017-02-04 14:58:26 +0100

    Improve message rendering and split into two lines

--- src/Message/Markdown.hs
+++ src/Message/Markdown.hs
@@ -7,13 +7,19 @@ module Message.Markdown
     , mr
     , ma
     , tl
+    , nl
 
     , italic
     , bold
     , codeblock
+    , blockquote
     , link
+    , h1, h2, h3, h4, h5
+
+    , cmb
     ) where
 
+import           Data.Maybe
 import           Data.Monoid
 import           Data.String (IsString)
 
@@ -23,7 +29,7 @@ type Stringish s = (Monoid s, IsString s, Eq s)
 
 -- | Text as a codeblock
 codeblock :: Stringish m => m -> m
-codeblock = nonNull $ \text -> "```" <> text <> "```"
+codeblock = ta "```"
 
 -- | Linkified text
 link :: Stringish m => m -> m -> m
@@ -31,15 +37,19 @@ link text url = nonNull (\text' -> "[" <> text' <> "](" <> url <> ")") text
 
 -- | Text put in quotes
 quote :: Stringish m => m -> m
-quote = nonNull $ \text -> "\"" <> text <> "\""
+quote = ta "\""
 
 -- | Text in italics
 italic :: Stringish m => m -> m
-italic = nonNull $ \text -> "*" <> text <> "*"
+italic = ta "*"
 
 -- | Text in bold
 bold :: Stringish m => m -> m
 bold = italic . italic
+
+-- | Block quotes
+blockquote :: Stringish m => m -> m
+blockquote = tl "> "
 
 -- | Margin left
 ml :: Stringish m => m -> m
@@ -51,7 +61,7 @@ mr = tr " "
 
 -- | Margin right
 ma :: Stringish m => m -> m
-ma = mr . ml
+ma = ta " "
 
 tl :: Stringish m => m -> m -> m
 tl prefix = nonNull $ \text -> prefix <> text
@@ -59,6 +69,30 @@ tl prefix = nonNull $ \text -> prefix <> text
 tr :: Stringish m => m -> m -> m
 tr suffix = nonNull $ \text -> text <> suffix
 
+ta :: Stringish m => m -> m -> m
+ta surrfix = tl surrfix . tr surrfix
+
+h1 :: Stringish m => m -> m
+h1 = tl "# "
+
+h2 :: Stringish m => m -> m
+h2 = tl "## "
+
+h3 :: Stringish m => m -> m
+h3 = tl "### "
+
+h4 :: Stringish m => m -> m
+h4 = tl "#### "
+
+h5 :: Stringish m => m -> m
+h5 = tl "##### "
+
 -- | Applies f if m is non-empty
 nonNull :: Stringish m  => (m -> m) -> m -> m
 nonNull f m = if m == mempty then m else f m
+
+nl :: (IsString s) => s
+nl = "\n"
+
+cmb :: (Stringish s) => Maybe s -> s
+cmb = fromMaybe mempty
